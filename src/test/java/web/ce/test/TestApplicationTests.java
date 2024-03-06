3bd9ccc450278a9ceaf671e6c9be7270ac6ce1b0
Alpha-Lima-India <147832944+Alpha-Lima-India@users.noreply.github.com>
2024-03-06 15:21:41 +0330

    Add files via upload
    exercise 1, src files
    web developing

---
+++ src/test/java/web/ce/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package web.ce.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
